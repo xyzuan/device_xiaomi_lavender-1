6344f23029d0cfb8396ddb5f2acee06bc3e5d0c6
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    lavender: Build LiveDisplay HAL
    Change-Id: I1a1f630bd8c4670b3fe9c16d2493019f0aa56a95

--- device.mk
+++ device.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lavender
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
