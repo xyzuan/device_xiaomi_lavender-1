b312bb47ff3f931fef5667ee499a818df843e593
adiatul16 <adiatul16@gmail.com>
2020-09-22 22:34:11 +0530

    lavender: Include overlays

--- device.mk
+++ device.mk
@@ -16,3 +16,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+
