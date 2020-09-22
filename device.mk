78e6be44ab58386617eeb4483461d0850b724355
Demon000 <demonsingur@gmail.com>
2020-09-22 22:31:08 +0530

    lavender: Build separate system and vendor HIDL libs

--- device.mk
+++ device.mk
@@ -9,3 +9,10 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender
+
+#HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
