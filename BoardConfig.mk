3b01c55fa55bb75763bf8af0ffc1bca7be13ae59
adiatul16 <adiatul16@gmail.com>
2020-09-22 19:18:44 +0530

    lavender: Import manifest from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=16
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
