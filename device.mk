f94ea42232baa3756274d1e04d09cf48a93972d5
adiatul16 <adiatul16@gmail.com>
2020-09-22 21:50:46 +0530

    lavender: inherit from base device configuration

---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
