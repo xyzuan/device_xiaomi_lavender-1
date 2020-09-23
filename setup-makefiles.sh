e09c34bdf4ddeaf0037064e5bf6acedb5aa2dfa6
adiatul16 <adiatul16@gmail.com>
2020-09-23 13:34:19 +0530

    lavender: Add initial proprietary files
    * All blobs are extracted from lavender-user-10-QKQ1.190910.002-V12.0.1.0.QFGCNXM-release-keys

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,7 +21,7 @@ set -e
 DEVICE=lavender
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=**** FILL IN COPYRIGHT YEAR ****
+INITIAL_COPYRIGHT_YEAR=2019
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
